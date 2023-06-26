77287eab3916a3dca1dba30818e0d4f053e8d34c
Nathan4Andrade <nathanandrade20@gmail.com>
2023-06-26 15:41:03 -0300

    listas

--- src/app.js
+++ src/app.js
@@ -5,22 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const tweets = [
-  {
-    username: "bobesponja",
-    avatar:
-      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-    tweet: "Eu amo hambúrguer de siri!",
-  },
-];
+const tweets = [];
 
-const users = [
-  {
-    username: "bobesponja",
-    avatar:
-      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-  },
-];
+const users = [];
 
 app.post("/sign-up", (req, res) => {
   const newUser = req.body;
